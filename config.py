07175320a19501352f2544f37017adbd096b636a
bydylo <arquivos.dylo@gmail.com>
2024-12-26 16:08:24 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+# Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+# Your API ID from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "11680917"))
+
+# Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "71e0cc2fd9148dd5002a62df6c907587
+")
+
+# Your Owner / Admin Id For Broadcast 
+ADMINS = int(os.environ.get("ADMINS", ""))
+
+# Your Mongodb Database Url
+# Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
